e5bdb88d66c6a26225e136d1bd38148fc4d60542
Rohit Gupta <gshaily45@gmail.com>
2024-02-17 14:53:05 +0530

    optimize

--- components/Skills/CoursePage/Header/Header.js
+++ components/Skills/CoursePage/Header/Header.js
@@ -38,7 +38,34 @@ const Header = ({
   const [idBtnV, setIdBtnV] = useState("org-dv");
   console.log(redirectDs, redirectFs, redirectDa);
   return (
-    <div className="grid grid-cols-[60%,39%] max-sm:flex max-sm:flex-col gap-5 max-sm:mb-[750px] bg-[#111621] w-full mt-[70px] px-28 max-sm:px-5 py-[100px] pb-[70px] max-sm:py-14 relative">
+    <div className="grid grid-cols-[60%,39%] max-sm:flex max-sm:flex-col gap-5 max-sm:mb-[10px] bg-[#111621] w-full mt-[70px] min-[1600px]:mt-[70px] px-28 min-[1600px]:px-48 max-sm:px-5 py-[100px] min-[1600px]:py-[90px] pb-[70px] max-sm:pb-[30px] max-sm:py-4 relative">
+      <div className="flex gap-3 text-[#F18350] font-bold items-center max-sm:mb-[-8px] ">
+        <p className="text-[#F18350] font-semibold min-[1600px]:text-[20px] max-sm:text-[15px] max-sm:font-medium">
+          Self-Paced
+        </p>
+        <MdKeyboardArrowRight className="text-white min-[1600px]:text-[20px]" />
+        <p className="text-[#F18350] font-semibold max-sm:text-[15px] min-[1600px]:text-[20px] max-sm:font-medium">
+          {title}
+        </p>
+        {/* <MdKeyboardArrowRight className="text-[white]" />
+          <p>Data Science</p> */}
+      </div>
+      <div className="w-[110%] ml-[-19px] h-[280px] relative flex justify-center min-[482px]:hidden">
+        <div onClick={() => showVideo(true)} id={idBtnV}>
+          <Image
+            src={imgSrc}
+            alt="headerImg"
+            fill
+            priority
+            quality={40}
+            id={idBtnV}
+          />
+          <MdOutlinePlayCircleOutline
+            className="absolute z-10 text-7xl text-white left-[42%] top-[41%] cursor-pointer "
+            id={idBtnV}
+          />
+        </div>
+      </div>
       {show && (
         <VideoPlaylist
           setShow={showVideo}
@@ -51,17 +78,6 @@ const Header = ({
 
       <div className="absolute gradient top-0 left-0 h-full w-[60%] max-sm:w-full z-0"></div>
       <div className="flex flex-col gap-2 relative z-[1]">
-        <div className="flex gap-3 text-[#F18350] font-bold items-center max-sm:hidden">
-          <p className="text-[#F18350] font-semibold max-sm:text-[15px] max-sm:font-medium">
-            Self-Paced
-          </p>
-          <MdKeyboardArrowRight className="text-white" />
-          <p className="text-[#F18350] font-semibold max-sm:text-[15px] max-sm:font-medium">
-            {title}
-          </p>
-          {/* <MdKeyboardArrowRight className="text-[white]" />
-          <p>Data Science</p> */}
-        </div>
         {redirectDs && (
           <div className="px-4 py-[6px] mt-4 bg-white w-max rounded">
             {/* <Image src="https://skillslash-cdn.s3.ap-south-1.amazonaws.com/static/New_skillslash/Homepage/powered-by-microsoft-(White).png" alt="PoweredByMicrosoft" width={200} height={22}/> */}
@@ -69,8 +85,8 @@ const Header = ({
             <Image
               src="https://skillslash-cdn.s3.ap-south-1.amazonaws.com/NewDatascience/Header/Powered+by+microsoft+tag+webp.webp"
               alt="PoweredByMicrosoft"
-              width={200}
-              height={22}
+              width={255}
+              height={28}
               priority
             />
           </div>
@@ -90,10 +106,10 @@ const Header = ({
         <h1 className="text-5xl text-white font-bold leading-[58px] max-[1281px]:text-3xl max-sm:leading-[45px]">
           {title}
         </h1>
-        <p className="text-[#cccccc] w-[91%] text-[17px] max-sm:text-[16px] max-[1281px]:text-[16px] leading-[28px] max-sm:leading-[24px] font-light mt-2 max-sm:mt-0">
+        <p className="text-[#cccccc] w-[91%] min-[1600px]:w-[75%] text-[17px] max-sm:text-[16px] max-[1281px]:text-[16px] leading-[28px] max-sm:leading-[24px] font-light mt-2 max-sm:mt-0 min-[1600px]:text-[20px]">
           {desc}
         </p>
-        <div className="grid grid-cols-3 gap-2 border-[1px] mt-3 w-[75%] max-sm:w-[97%] rounded items-center px-4 py-1 justify-center">
+        <div className="grid grid-cols-3 gap-2 min-[1600px]:gap-0 border-[1px] mt-3 w-[75%] min-[1600px]:w-[60%] max-sm:w-[97%] rounded items-center px-4 py-1 justify-center">
           <Image
             src="https://skillslash-cdn.s3.ap-south-1.amazonaws.com/NewDatascience/googlereview-new.webp"
             alt="Goole_RReviews"
@@ -120,14 +136,17 @@ const Header = ({
           />
         </div>
         <div onClick={() => showVideo(true)}>
-          <button className="bg-[#f18350] px-3 py-2 mt-4" id={idBtnDV}>
+          <button
+            className="bg-[#f18350] px-3 py-2 mt-4 max-sm:hidden"
+            id={idBtnDV}
+          >
             Watch Demo Videos
           </button>
         </div>
       </div>
       <div className="flex flex-col gap-7 relative">
-        <div className="bg-white px-11 py-3 max-sm:px-0 rounded shadow flex flex-col w-full z-[1] absolute mt-28">
-          <div className="absolute w-[413px]  max-sm:w-[313px] h-[290px] max-sm:h-[220px] top-[-120px] max-sm:top-[-100px] left-12 min-[1440px]:left-8 max-sm:left-7 max-sm:flex max-sm:justify-center">
+        <div className="bg-white px-11 py-3 max-sm:px-0 rounded shadow flex flex-col w-full min-[1600px]:w-[90%] z-[1] max-sm:hidden absolute mt-28">
+          <div className="absolute w-[413px]  max-sm:w-[313px] h-[290px] max-sm:h-[220px] top-[-120px] max-sm:top-[-100px] left-12 min-[1600px]:left-20 max-sm:left-7 max-sm:flex max-sm:justify-center">
             <div onClick={() => showVideo(true)} id={idBtnV}>
               <Image
                 src={imgSrc}
