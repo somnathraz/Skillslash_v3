88e0ef4d4156a4af35c73ef456bd8b527678bfd4
somnath khadanga <somnathkhadanga@somnaths-MacBook-Pro.local>
2024-02-19 15:35:11 +0530

    hello

--- pages/api/getData.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { connectToDatabase } from "../../lib/mongodb";
-export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
-  let myPost = await db.collection("getInfo").insertOne(req.body);
-  console.log(req.body);
-  res.status(200).json({ name: "done" });
-}
