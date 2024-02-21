b19cabc1ec1c850f444f203445588b251bbc0179
somnath khadanga <somnathkhadanga@somnaths-MacBook-Pro.local>
2024-02-21 13:05:44 +0530

    new changes

---
+++ pages/api/checkRegion.js
@@ -0,0 +1,14 @@
+import geoip from "geoip-lite";
+
+export default async function handler(req, res) {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const geo = geoip.lookup(ip);
+
+  if (geo && geo.country === "NG") {
+    // 'NG' is the ISO code for Nigeria
+    res.status(200).json({ allowed: true });
+  } else {
+    // Redirect or send error, depending on your preference
+    res.status(502).json({ allowed: false }); // Redirect example
+  }
+}
