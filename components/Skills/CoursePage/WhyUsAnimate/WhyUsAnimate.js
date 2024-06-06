c2ea128c6bd3093922629f980e9673a51b50edf7
amit <amit@learnbay.co>
2024-06-07 03:11:06 +0530

    small chnages 2 time

--- components/Skills/CoursePage/WhyUsAnimate/WhyUsAnimate.js
+++ components/Skills/CoursePage/WhyUsAnimate/WhyUsAnimate.js
@@ -154,7 +154,7 @@ const WhyUsAnimate = ({ redirectDa, redirectDs, home, redirectFs, nomicrosoft , 
         {dmPage ? (<><div className="bg-black shadow rounded-lg px-7 py-4 flex flex-col gap-3 min-[481px]:w-[75%]">
                       <div className="relative w-full h-[180px]  border-[1px] border-white ">
                         <Image
-                          src={imageSrcDM[1]}
+                          src={imageSrcDM[2]}
                           fill
                           alt="certificate image"
                           loading="lazy"
@@ -168,7 +168,7 @@ const WhyUsAnimate = ({ redirectDa, redirectDs, home, redirectFs, nomicrosoft , 
                       </p>
                     </div></>):(<>      <div className="bg-black shadow rounded-lg px-7 py-4 flex flex-col gap-3 min-[481px]:w-[75%]">
                 <div className="relative w-full h-[180px]  border-[1px] border-white">
-                  <Image src={imageSrc[0]} fill alt="certificate image"  loading="lazy"/>
+                  <Image src={imageSrc[1]} fill alt="certificate image"  loading="lazy"/>
                 </div>
                 <h3 className="font-bold text-[#f18350]">
                   {" "}
