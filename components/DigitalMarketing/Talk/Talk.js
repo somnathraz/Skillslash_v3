220d8c5dae31e72bfa21be1d1747a6ca9d1d2fba
amit <amit@learnbay.co>
2024-06-10 10:59:36 +0530

    talk

--- components/DigitalMarketing/Talk/Talk.js
+++ components/DigitalMarketing/Talk/Talk.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './Talk.module.css'; // Import the CSS module
-
 import { FiPhoneCall } from "react-icons/fi";
 import Link from 'next/link';
 
-
 const Talk = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    
+    handleResize();
+
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <>
     <div className={styles.talkHead}>
       <div className={styles.talkMain}>
         <div className={styles.backgroundImageWrapper}>
           <Image 
-            src="https://skillslash-cdn.s3.ap-south-1.amazonaws.com/digital-marketing/Group+908.webp"
+            src={isMobile 
+              ? "https://skillslash-cdn.s3.ap-south-1.amazonaws.com/digital-marketing/mbl_talk.webp"
+              : "https://skillslash-cdn.s3.ap-south-1.amazonaws.com/digital-marketing/Group+908.webp"}
             layout="fill"
             objectFit="cover"
             alt='bg img'
@@ -21,17 +37,16 @@ const Talk = () => {
           />
         </div>
         <div className={styles.content}>
-      <span className={styles.gotspan}>Got more questions?</span>
-      <h5 className={styles.teamh5}>Talk to our team directly</h5>
-      <p className={styles.para}>Contact us and our academic counsellor will get in touch
-with you shortly</p>
-<Link href="tel:+918391911911" className={styles.btn}>
+          <span className={styles.gotspan}>Got more questions?</span>
+          <h5 className={styles.teamh5}>Talk to our team directly</h5>
+          <p className={styles.para}>Contact us and our academic counsellor will get in touch
+          with you shortly</p>
+          <Link href="tel:+918391911911" className={styles.btn}>
             <FiPhoneCall className={styles.icon}/> +91 8391-911-911
           </Link>
         </div>
       </div>
-      </div>
-    </>
+    </div>
   );
 }
 
