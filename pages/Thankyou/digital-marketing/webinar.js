15b66a91fafec2eef7ba20383774058d362f2d8b
amit <amit@learnbay.co>
2024-06-21 19:53:32 +0530

    button link

--- pages/Thankyou/digital-marketing/webinar.js
+++ pages/Thankyou/digital-marketing/webinar.js
@@ -2,8 +2,22 @@ import Head from 'next/head'
 import { MdEmail } from 'react-icons/md'
 import Navbar from '../../../components/Navbar/Navbar'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function ThankYou() {
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText('https://us06web.zoom.us/j/81451214111')
+      .then(() => {
+        setCopySuccess('Link copied!');
+        setTimeout(() => setCopySuccess(''), 3000); // Clear the message after 3 seconds
+      })
+      .catch(err => {
+        setCopySuccess('Failed to copy');
+      });
+  };
+
   return (
     <>
       <Head>
@@ -17,16 +31,20 @@ export default function ThankYou() {
           <MdEmail className="text-8xl text-gray-700 mb-4 animate-slideUp" />
           <h1 className="text-2xl font-bold text-center text-gray-900">Congratulation! you have successfully registered for Workshop</h1>
           <p className="mt-4 text-gray-700 text-center">
-          Please save the below link and use the same to join on 22nd June at 11 AM.
-
-
-    
+            Please save the below link and use the same to join on 22nd June at 11 AM.
           </p>
           <br/>
           <div className='m-2 flex flex-row items-center max-sm:flex-col '>
-<b>Link:</b>  <Link className="text-blue-500 ml-2" href="https://us06web.zoom.us/j/81451214111" target='_blank'>https://us06web.zoom.us/j/81451214111</Link>
-</div>
-
+            <b>Link:</b>  
+            <Link className="text-blue-500 ml-2" href="https://us06web.zoom.us/j/81451214111" target='_blank'>https://us06web.zoom.us/j/81451214111</Link>
+          </div>
+          <button 
+            onClick={copyToClipboard} 
+            className="mt-4 text-white bg-blue-500 rounded-lg px-4 py-1 hover:bg-blue-600"
+          >
+            Copy Link
+          </button>
+          {copySuccess && <p className="mt-2 text-green-500">{copySuccess}</p>}
           <div className="mt-6 text-center">
             <a
               href="https://skillslash.com/liveclass/digital-marketing-master-course"
